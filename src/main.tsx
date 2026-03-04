918136c21aae5321b5095f0bff6101ee7b3b0f5a
rubin <rubin666666666@gmail.com>
2026-03-04 13:02:35 +0200

    chore: stop tracking local artifacts and keep .history local

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
 import "modern-normalize"
+import "./index.css"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+)
